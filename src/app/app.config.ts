5c8cf73281acecba2f538d48d80f4cba0a85914c
Diego <sdieg82@gmail.com>
2025-09-18 21:22:59 -0500

    final

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -7,7 +7,7 @@ export const appConfig: ApplicationConfig = {
   providers: [
     provideBrowserGlobalErrorListeners(),
     provideZoneChangeDetection({ eventCoalescing: true }),
-    provideRouter(routes),
-    provideRouter(routes, withHashLocation())
+    provideRouter(routes,withHashLocation())
   ]
 };
+
